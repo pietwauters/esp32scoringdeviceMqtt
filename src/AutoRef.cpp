3b5f66b62c288ce96d7b2c0be39bbfb507f6cd9a
pietwauters <67114377+pietwauters@users.noreply.github.com>
2026-04-10 17:06:07 +0200

    Fix bug with no autostart after reset

--- src/AutoRef.cpp
+++ src/AutoRef.cpp
@@ -271,7 +271,7 @@ void AutoRef::handleLongPress(uint32_t lhdEvent, uint32_t now) {
     strip.ClearAll();
     strip.startAnimation(EVENT_WS2812_AUTOREF_MODE);
     setEnabled(true);
-    m_state = AR_ARMED;
+    m_state = AR_AWARDING;
     m_stateEnteredAt = now;
     m_prevLights = 0;
     m_peakLights = 0;
@@ -283,7 +283,7 @@ void AutoRef::handleLongPress(uint32_t lhdEvent, uint32_t now) {
   strip.ClearAll();
   strip.startAnimation(EVENT_WS2812_AUTOREF_MODE);
   sendToFSM(EVENT_UI_INPUT | UI_INPUT_RESET);
-  m_state = AR_ARMED;
+  m_state = AR_AWARDING;
   m_stateEnteredAt = now;
   m_prevLights = 0;
   m_peakLights = 0;
